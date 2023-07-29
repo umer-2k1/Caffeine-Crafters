f9b84ea99f76ec789af3d30a2d9c06f8ca987d3f
umermemon4648 <umermemon4648@gmail.com>
2023-07-29 12:39:48 +0500

    Initial Setup for React Native app

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+import React from "react";
+const Layout: React.FC = () =>{
+    return(
+        <>
+        <Stack/>
+        </>
+    )
+}
+
+export default Layout
