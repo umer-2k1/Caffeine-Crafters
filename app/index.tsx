ac6cd3e11951344392122a1a2ec5fdb9460f1768
umermemon4648 <umermemon4648@gmail.com>
2023-07-29 13:09:44 +0500

    Initail React Native app setup

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from 'react'
+const Home: React.FC = ()=>{
+    return(
+        <>
+        <SafeAreaView>
+            <View>
+                <Text>Hello Umer</Text>
+                <Text>Hello Umer</Text>
+                <Text>Hello Umer</Text>
+            </View>
+        </SafeAreaView>
+        </>
+    )
+}
+export default Home
