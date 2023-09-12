4edb1a7f0b4bbf1928a5913a989b9a79f1c5d645
Umer Memon <117743102+umermemon4648@users.noreply.github.com>
2023-09-12 11:55:51 +0500

    resolved issues

--- screen/Home.tsx
+++ screen/Home.tsx
@@ -1,19 +1,40 @@
-// import React from 'react'
-// import { View, Image, StatusBar } from 'react-native'
-// const Home = () => {
-//   return (
-//     <>
-//     <View className='flex-1 bg-pink-600 relative'>
-//         <StatusBar/>
-//         <Image
-//         source={require('../assets/images/beansBackground1.png')}
-//         className='w-full absolute opacity-10 -top-5 bg-white'
-//         style={{height:420}}
-//         />
-//     </View>
-    
-//     </>
-//   )
-// }
+import React from 'react'
+import { View, Image, StatusBar, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'twrnc';
+// import {MapPinIcon} from 'react-native-heroicons/solid'
+import { FaMapMarkerAlt } from 'react-icons/fa';
+// import { SparklesIcon } from "react-native-heroicons/mini";
+const Home = () => {
+  return (
+    <>
+    <View style={tw `flex-1 bg-pink-600 relative`} className=''>
+        <StatusBar/>
+        <Image
+        source={require('../assets/images/beansBackground1.png')}
+        style={tw `flex-1 bg-pink-600 relative w-full absolute opacity-20 -top-5 bg-white h-[200px]`}
+        />
 
-// export default Home
+        {/* avatar and bell icon */}
+        <SafeAreaView style={tw `flex-1 absolute`} >
+
+          <View style={tw `px-4 flex-row justify-between items-center`} >
+            <Image
+            source={require('../assets/images/avatar.png')}
+            style={tw `h-[36px] w-[36px] rounded-full`}
+            />
+            <View style={tw `flex-row items-center`}>
+              {/* <MapPinIcon /> */}
+              {/* <FaMapMarkerAlt style={tw `h-[50px] w-[50px] `} /> */}
+              {/* <SparklesIcon /> */}
+              <Text style={tw `text-black text-4xl`}>React Native</Text>
+            </View>
+          </View>
+        </SafeAreaView>
+    </View>
+    
+    </>
+  )
+}
+
+export default Home
