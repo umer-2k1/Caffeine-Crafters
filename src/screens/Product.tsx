e0859e9778f0815f203946bfa15103ec69de2c04
Umer Memon <117743102+umermemon4648@users.noreply.github.com>
2023-09-29 14:57:45 +0500

    Update Product.tsx

--- src/screens/Product.tsx
+++ src/screens/Product.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
@@ -6,9 +6,21 @@ import { StatusBar } from "expo-status-bar";
 import { Feather } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 import { themeColors } from "../../themes";
+import { AntDesign } from "@expo/vector-icons";
+import { SimpleLineIcons } from "@expo/vector-icons";
 const Product = (props: any) => {
   const item = props.route.params;
   const navigation = useNavigation();
+  const [size, setSize] = useState("Small");
+  const [quantity, setQuantity] = useState(1);
+  const incrementQuantity = (quantity: number) => {
+    setQuantity(quantity + 1);
+  };
+  const decrementQuantity = (quantity: number) => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
   return (
     <View className="flex-1">
       <StatusBar style="light" />
@@ -22,7 +34,7 @@ const Product = (props: any) => {
         className="w-full absolute"
       />
       <SafeAreaView className="space-y-4">
-        <View className="mx-4 my-2 flex-row items-center justify-between">
+        <View className="mx-4 my-1 flex-row items-center justify-between">
           <TouchableOpacity onPress={() => navigation.goBack()}>
             <Feather name="arrow-left-circle" size={50} color="white" />
           </TouchableOpacity>
@@ -46,13 +58,135 @@ const Product = (props: any) => {
         </View>
         {/* details */}
         <View
-          style={{ backgroundColor: "rgba(255,255,255,0.2)" }}
-          className="flex-row items-center rounded-full px-2 p-2 mt-2 space-x-2 w-20"
+          style={{ backgroundColor: themeColors.bgLight }}
+          className="mx-4 flex-row items-center rounded-full p-2 px-4 space-x-2 w-24"
         >
           <Ionicons name="star" size={18} color="white" />
           <Text className="text-white text-base font-semibold">
             {item.stars}
           </Text>
+        </View>
+        {/* name and price */}
+        <View className="mx-4 flex-row justify-between items-center">
+          <Text
+            style={{ color: themeColors.text }}
+            className="text-3xl font-bold "
+          >
+            {item.name}
+          </Text>
+          <Text
+            style={{ color: themeColors.text }}
+            className="text-lg font-bold"
+          >
+            ${item.price}
+          </Text>
+        </View>
+        {/* size */}
+        <View className="mx-4 space-y-1">
+          <Text
+            style={{ color: themeColors.text }}
+            className="text-lg font-bold"
+          >
+            Coffee size
+          </Text>
+          <View className="flex-row justify-between">
+            <TouchableOpacity
+              onPress={() => setSize("small")}
+              style={{
+                backgroundColor:
+                  size === "small" ? themeColors.bgLight : "rgba(0,0,0,0.07)",
+              }}
+              className="p-3 px-8 rounded-full"
+            >
+              <Text
+                className={
+                  size === "small" ? "text-white" : "text-gray-700 font-medium"
+                }
+              >
+                Small
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setSize("medium")}
+              style={{
+                backgroundColor:
+                  size === "medium" ? themeColors.bgLight : "rgba(0,0,0,0.07)",
+              }}
+              className="p-3 px-8 rounded-full"
+            >
+              <Text
+                className={
+                  size === "medium" ? "text-white" : "text-gray-700 font-medium"
+                }
+              >
+                Medium
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setSize("large")}
+              style={{
+                backgroundColor:
+                  size === "large" ? themeColors.bgLight : "rgba(0,0,0,0.07)",
+              }}
+              className="p-3 px-8 rounded-full"
+            >
+              <Text
+                className={
+                  size === "large" ? "text-white" : "text-gray-700 font-medium"
+                }
+              >
+                Large
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        {/* about and desc */}
+        <View className="mx-4 space-y-2">
+          <Text
+            style={{ color: themeColors.text }}
+            className="text-lg font-bold"
+          >
+            About
+          </Text>
+          <Text style={{ color: themeColors.text }} className="text-gray-700">
+            {item.desc}
+          </Text>
+        </View>
+        <View className="mx-4 flex-row justify-between items-center">
+          <View className="flex-row items-center space-x-1">
+            <Text className="text-lg text-gray-700 font-semibold opacity-60">
+              Volume
+            </Text>
+            <Text className="text-lg text-black font-bold ">{item.volume}</Text>
+          </View>
+          <View className="flex-row items-center space-x-4 border-2 border-gray-500 rounded-full p-1 px-4">
+            <TouchableOpacity onPress={() => decrementQuantity(quantity)}>
+              <AntDesign name="minus" size={20} color="black" />
+            </TouchableOpacity>
+            <Text
+              style={{ color: themeColors.text }}
+              className="font-extrabold text-base"
+            >
+              {quantity}
+            </Text>
+            <TouchableOpacity onPress={() => incrementQuantity(quantity)}>
+              <AntDesign name="plus" size={20} color="black" />
+            </TouchableOpacity>
+          </View>
+        </View>
+        {/* buy now button */}
+        <View className="mx-4 flex-row justify-between items-center">
+          <TouchableOpacity className="rounded-full border border-gray-500 p-3">
+            <SimpleLineIcons name="handbag" size={30} color="gray" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{ backgroundColor: themeColors.bgLight }}
+            className="p-[16px] rounded-full flex-1 ml-3"
+          >
+            <Text className="text-center text-lg font-bold text-white">
+              Buy now
+            </Text>
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     </View>
