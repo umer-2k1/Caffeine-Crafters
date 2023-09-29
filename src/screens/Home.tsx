79f3346635f9e22d2008999902eb9a85b5d5328c
Umer Memon <117743102+umermemon4648@users.noreply.github.com>
2023-09-29 18:27:08 +0500

    Update Home.tsx

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,6 +7,9 @@ import {
   TextInput,
   TouchableOpacity,
   FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CoffeeCard from "../components/CoffeeCard";
@@ -20,98 +23,112 @@ export default function Home() {
   const navigate = useNavigation();
   const [activeCategory, setActiveCategory] = useState(1);
   return (
-    <View className="flex-1 relative bg-white">
-      <StatusBar />
-      <Image
-        source={require("../assets/images/beansBackground1.png")}
-        className="flex-1 w-full absolute opacity-20 -top-5 h-[200px]"
-      />
-
-      {/* avatar and bell icon */}
-      <SafeAreaView className="flex justify-between">
-        <View className="mx-4 flex-row justify-between items-center">
-          <Image
-            source={require("../assets/images/avatar.png")}
-            className="h-[36px] w-[36px] rounded-full"
-          />
-
-          <View className="justify-between flex-row items-center space-x-2">
-            <FontAwesome
-              name="map-marker"
-              size={25}
-              color={themeColors.bgLight}
-            />
-            <Text className="text-base font-semibold">Karachi, Pakistan</Text>
-          </View>
-          <Feather name="bell" size={28} color="black" />
-        </View>
-
-        {/* search */}
-        <View className="mx-5 mt-12 shadow">
-          <View className="flex-row items-center rounded-full p-1 bg-[#e6e6e6]">
-            <TextInput
-              placeholder="Search"
-              className="p-4 flex-1 font-semibold text-gray-700"
-            />
-            <TouchableOpacity
-              className="rounded-full p-2"
-              style={{ backgroundColor: themeColors.bgLight }}
-            >
-              <Feather name="search" size={24} color="white" />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </SafeAreaView>
-      {/* categories */}
-      <View className="px-5 mt-6">
-        <FlatList
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          data={categories}
-          keyExtractor={(item) => item.id.toString()}
-          className="overflow-visible"
-          renderItem={({ item }) => {
-            const isActive = item.id === activeCategory;
-            const activeTextClass = isActive ? "text-[#fff]" : "text-gray-700";
-
-            return (
-              <TouchableOpacity
-                style={{
-                  backgroundColor: isActive
-                    ? themeColors.bgLight
-                    : "rgba(0, 0, 0, 0.07)",
-                }}
-                className="p-4 px-5 rounded-full mr-2 shadow "
-                onPress={() => setActiveCategory(item.id)}
-              >
-                <Text className={`font-semibold ${activeTextClass}`}>
-                  {item.title}
-                </Text>
-              </TouchableOpacity>
-            );
-          }}
+    <>
+      {/* <View> */}
+      <View className="flex-1 relative bg-white">
+        {/* <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        > */}
+        <StatusBar />
+        <Image
+          source={require("../assets/images/beansBackground1.png")}
+          className="flex-1 w-full absolute opacity-20 -top-5 h-[200px]"
         />
-      </View>
-      {/* Carousel */}
-      <View
-        // style={{ backgroundColor: "transparent", zIndex: -1 }}
-        className="z-999 flex flex-1 justify-center space-y-2"
-      >
-        <View className="">
-          <Carousel
-            containerCustomStyle={{ overflow: "visible" }}
-            data={coffeeItems}
-            renderItem={({ item }) => <CoffeeCard item={item} />}
-            firstItem={1}
-            inactiveSlideOpacity={0.75}
-            inactiveSlideScale={0.75}
-            loop={true}
-            sliderWidth={400} //actual slide width
-            itemWidth={250} //card width
-            slideStyle={{ display: "flex", alignItems: "center" }}
+        {/* <ScrollView> */}
+
+        {/* avatar and bell icon */}
+        <SafeAreaView className="flex justify-between">
+          <View className="mx-4 flex-row justify-between items-center">
+            <Image
+              source={require("../assets/images/avatar.png")}
+              className="h-[36px] w-[36px] rounded-full"
+            />
+
+            <View className="justify-between flex-row items-center space-x-2">
+              <FontAwesome
+                name="map-marker"
+                size={25}
+                color={themeColors.bgLight}
+              />
+              <Text className="text-base font-semibold">Karachi, Pakistan</Text>
+            </View>
+            <Feather name="bell" size={28} color="black" />
+          </View>
+
+          {/* search */}
+          <View className="mx-5 mt-12 shadow">
+            <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+            <View className="flex-row items-center rounded-full p-1 bg-[#e6e6e6]">
+              <TextInput
+                placeholder="Search"
+                className="p-4 flex-1 font-semibold text-gray-700"
+              />
+              <TouchableOpacity
+                className="rounded-full p-2"
+                style={{ backgroundColor: themeColors.bgLight }}
+              >
+                <Feather name="search" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </SafeAreaView>
+          <KeyboardAvoidingView/>
+        {/* categories */}
+        <View className="px-5 mt-6">
+          <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={categories}
+            keyExtractor={(item) => item.id.toString()}
+            className="overflow-visible"
+            renderItem={({ item }) => {
+              const isActive = item.id === activeCategory;
+              const activeTextClass = isActive
+                ? "text-[#fff]"
+                : "text-gray-700";
+
+              return (
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: isActive
+                      ? themeColors.bgLight
+                      : "rgba(0, 0, 0, 0.07)",
+                  }}
+                  className="p-4 px-5 rounded-full mr-2 shadow "
+                  onPress={() => setActiveCategory(item.id)}
+                >
+                  <Text className={`font-semibold ${activeTextClass}`}>
+                    {item.title}
+                  </Text>
+                </TouchableOpacity>
+              );
+            }}
           />
         </View>
+        {/* Carousel */}
+        <View
+          // style={{ backgroundColor: "transparent", zIndex: -1 }}
+          className="z-999 flex flex-1 justify-center space-y-2"
+        >
+          <View className="">
+            <Carousel
+              containerCustomStyle={{ overflow: "visible" }}
+              data={coffeeItems}
+              renderItem={({ item }) => <CoffeeCard item={item} />}
+              firstItem={1}
+              inactiveSlideOpacity={0.75}
+              inactiveSlideScale={0.75}
+              loop={true}
+              sliderWidth={400} //actual slide width
+              itemWidth={250} //card width
+              slideStyle={{ display: "flex", alignItems: "center" }}
+            />
+          </View>
+        </View>
+        {/* </ScrollView> */}
       </View>
-    </View>
+    </>
   );
 }
